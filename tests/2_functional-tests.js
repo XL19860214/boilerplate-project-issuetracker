ac01b17bd09b7c9cf8e46665613e059dbc777379
Xuwei Li <72472538+XL19860214@users.noreply.github.com>
2021-08-25 19:56:19 +0000

    Add test 'View issues on a project: GET request to /api/issues/{project}'.

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -26,6 +26,31 @@ suite('Functional Tests', function() {
     const testObject = {
       issue_title: lorem.generateWords(Math.ceil(Math.random() * 10)),
       issue_text: lorem.generateParagraphs(Math.ceil(Math.random() * 5)),
+      created_by: humanNames.allRandom(),
+      assigned_to: humanNames.allRandom(),
+      status_text: lorem.generateWords(lorem.generateSentences(1))
+    }
+
+    chai.request(server)
+        .post('/api/issues/apitest')
+        .type('form')
+        .send(testObject)
+        .end((err, res) => {
+          assert.equal(err, null);
+          assert.equal(res.status, 200);
+          const resObject = JSON.parse(res.text);
+          for (const [key, value] of Object.entries(testObject)) {
+            assert.equal(resObject[key], value);
+          }
+          done();
+        });
+  });
+
+  // #2
+  test('Create an issue with only required fields: POST request to /api/issues/{project}', done => {
+    const testObject = {
+      issue_title: lorem.generateWords(Math.ceil(Math.random() * 10)),
+      issue_text: lorem.generateParagraphs(Math.ceil(Math.random() * 5)),
       created_by: humanNames.allRandom()
     }
 
@@ -43,5 +68,48 @@ suite('Functional Tests', function() {
           done();
         });
   });
+
+  // #3
+  test('Create an issue with missing required fields: POST request to /api/issues/{project}', done => {
+    const testObject = {
+      issue_title: Math.random() >= 0.5 ? lorem.generateWords(Math.ceil(Math.random() * 10)) : undefined,
+      issue_text: Math.random() >= 0.5 ? lorem.generateParagraphs(Math.ceil(Math.random() * 5)) : undefined,
+      created_by: Math.random() >= 0.5 ? humanNames.allRandom() : undefined
+    }
+
+    chai.request(server)
+        .post('/api/issues/apitest')
+        .type('form')
+        .send(testObject)
+        .end((err, res) => {
+          assert.equal(err, null);
+          assert.equal(res.status, 200);
+          const resObject = JSON.parse(res.text);
+          assert.equal(resObject.error, 'required field(s) missing');
+          done();
+        });
+  });
+
+  // #4
+  test('View issues on a project: GET request to /api/issues/{project}', done => {
+    chai.request(server)
+        .get('/api/issues/apitest')
+        .send()
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          const resObjects = JSON.parse(res.text);
+          assert.isArray(resObjects);
+          resObjects.forEach(issue => {
+            assert.containsAllKeys(issue, [
+              '_id',
+              'issue_title',
+              'issue_text',
+              'created_by'
+            ]);
+          });
+          done();
+        });
+  });
+
 
 });
